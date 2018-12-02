397a0f2e7a071b13d27c842f59d8f2fe79faac7a
Benjamin Koch <bjckoch@gmail.com>
2018-12-02 12:34:37 -0600

    testing appointments and navigation using routermodule

--- EMIS/src/app/app.module.ts
+++ EMIS/src/app/app.module.ts
@@ -3,6 +3,27 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+// order matters, be very careful adding in newer/general terms
+{ path: 'login', component: LoginComponent },
+//{ path: 'appointment', component: AppointmentComponent },
+/*{ path: 'hero/:id',      component: HeroDetailComponent },
+  {
+    path: 'heroes',
+    component: HeroListComponent,
+    data: { title: 'Heroes List' }
+  },
+  */
+{
+	path: '',
+	redirectTo: '/login',
+	pathMatch: 'full'
+},
+//{ path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
@@ -11,9 +32,14 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
-	ReactiveFormsModule
+	ReactiveFormsModule,
+	AppRoutingModule, 
+	RouterModule.forRoot( appRoutes, 
+	{enableTracing: true}	// for debugging purposes only
+	)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
