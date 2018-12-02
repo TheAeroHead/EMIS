5b81a71a043dbfaef83b157438194299d43fd818
Benjamin Koch <bjckoch@gmail.com>
2018-12-02 13:11:18 -0600

    attempts to fix files

--- EMIS/src/app/app.module.ts
+++ EMIS/src/app/app.module.ts
@@ -4,31 +4,13 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { AppRoutingModule } from './app-routing.module';
-import { RouterModule, Routes } from '@angular/router';
-
-const appRoutes: Routes = [
-// order matters, be very careful adding in newer/general terms
-{ path: 'login', component: LoginComponent },
-//{ path: 'appointment', component: AppointmentComponent },
-/*{ path: 'hero/:id',      component: HeroDetailComponent },
-  {
-    path: 'heroes',
-    component: HeroListComponent,
-    data: { title: 'Heroes List' }
-  },
-  */
-{
-	path: '',
-	redirectTo: '/login',
-	pathMatch: 'full'
-},
 //{ path: '**', component: PageNotFoundComponent }
 import { AppointmentComponent } from './appointment/appointment.component';
 import { RouterModule, Routes } from '@angular/router';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 import {MatInputModule} from '@angular/material/input';
-//import 'hammerjs';
+//import 'hammer.js';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AuthServiceService } from './auth-service.service';
 import { AuthGuard } from './auth.guard';
@@ -38,7 +20,8 @@ const appRoutes: Routes = [
   { path: 'appointment', component: AppointmentComponent },
   { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
-  { path: 'logout', component: LoginComponent }
+  { path: 'logout', component: LoginComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
 
 @NgModule({
