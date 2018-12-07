f0f9ce0989647791e0a7e0496659d261d7a3e183
Michael Reyes <michaelreyes1107@gmail.com>
2018-12-07 15:36:40 -0600

    fixed bugs

--- EMIS/src/app/doctor/doctor.component.ts
+++ EMIS/src/app/doctor/doctor.component.ts
@@ -6,6 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./doctor.component.css']
 })
 export class DoctorComponent implements OnInit {
+  step = 0;
+
+  setStep(index: number) {
+    this.step = index;
+  }
+
+  nextStep() {
+    this.step++;
+  }
+
+  prevStep() {
+    this.step--;
+  }
 
   constructor() { }
 
